72aafafcd7c53f3e6452a9b52e7d7edb6d91d87c
fefealfonsi <fefealfonsi@gmail.com>
2020-12-08 10:38:52 -0300

    estilização do loading em andamento

--- src/screens/LoadingPage/LoadingPage.js
+++ src/screens/LoadingPage/LoadingPage.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+import logo from '../../assets/white-logo.svg'
+  
+const LoadingContainer=styled.div`
+  width: 360px;
+  height: 640px;
+  padding: 288px 117px 287px;
+  background-color: #000000;
+`
+
+const AllLoadingContainer=styled.div`
+  display:flex;
+  justify-content:center;
+`
 
 const LoadingPage = () => {
   return (
-    <div>
-      
-    </div>
+    <AllLoadingContainer>
+      <LoadingContainer>
+        <img src={logo} alt={"logo do Future Eats"}/>
+      </LoadingContainer>
+    </AllLoadingContainer>
   )
 }
 
