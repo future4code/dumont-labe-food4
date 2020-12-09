82483b5aa78256ba6114b9d97b15acea404b8ce2
Nicole <nzolnier@gmail.com>
2020-12-09 17:28:46 -0300

    search funcionando porem nao do jeito que eh pra ser

--- src/screens/FeedPage/FeedPage.js
+++ src/screens/FeedPage/FeedPage.js
@@ -1,17 +1,33 @@
+import { FormControl, InputAdornment, OutlinedInput } from '@material-ui/core'
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import RestaurantCard from '../../components/RestaurantCard'
 import { BASE_URL } from '../../constants/urls'
 import { useRequestData } from '../../hooks/useRequestData'
 import {FeedContainer,AllFeed } from './styles'
+import SearchIcon from '@material-ui/icons/Search';
+import {goToSearch} from '../../router/coordinator'
+import { useHistory } from 'react-router-dom'
 
 const FeedPage = () => {
   const getRestaurants = useRequestData(`${BASE_URL}/restaurants`, undefined)
-  console.log(getRestaurants)
+  const history = useHistory()
 
   return (
     <AllFeed> 
         <NavBar />
+        <div>
+        <FormControl variant="outlined">
+        <OutlinedInput
+          onClick={() => goToSearch(history)}
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+        </div>
         <FeedContainer>
           {/* <RestaurantCardContainer> */}
             {getRestaurants && getRestaurants.restaurants.map((restaurant) => {
