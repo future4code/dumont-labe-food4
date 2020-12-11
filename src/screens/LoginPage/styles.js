648662875c96b2c7710ea651cada10a1ce3e042f
Nicole <nzolnier@gmail.com>
2020-12-11 20:39:02 -0300

    mais bugfix

--- src/screens/LoginPage/styles.js
+++ src/screens/LoginPage/styles.js
@@ -14,10 +14,8 @@ export const LoginContainer = styled.div`
 export const FormContainer = styled.form`
     display: flex;
     flex-direction: column;
-    height: 28vh;
-    justify-content: space-around;
-    margin-bottom: 15px;
     gap: 16px;
+    justify-content: space-around;
 `
 
 export const Logo = styled.img`
@@ -35,4 +33,8 @@ export const Button = styled.button`
   border: none;
   background-color: #5cb646;
   margin: auto;
+`
+
+export const NoLogin = styled.div`
+    padding-top: 1em;
 `
